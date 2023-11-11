b0ecb5f9344ea03360ae528ee32c483385db4540
Kieran <kieranlukejervis@gmail.com>
2023-11-11 05:45:41 +0000

    Seperated token into settings file for hosting purposes

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ async def search(interaction, book_title: str, language: discord.app_commands.Ch
     if len(results) == 0:
         await interaction.response.send_message("No results found!")
     else:
-        await interaction.response.send_message(f"Found {len(results)} results!", view=SelectView())
+        await interaction.response.send_message(f"Found {len(results)} results!", view=SelectView(), ephemeral=True)
 
 client.run(TOKEN)
 
